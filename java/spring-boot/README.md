12b3c48f731df5b801b86bbbc750bbef4e3fcf47
weibangtuo <weibangtuo@scutech.com>
2019-05-21 17:55:14 +0800

    Add spring-boot webhook example

---
+++ java/spring-boot/README.md
@@ -0,0 +1,8 @@
+# WebHook Example
+
+## Files
+
+- pom.xml
+- src/main/java/com/scutech/dbackup/webhook/Application.java
+- src/main/java/com/scutech/dbackup/webhook/WebHookController.java
+- src/main/resources/application.properties
---
+++ java/spring-boot/src/main/java/com/scutech/dbackup/webhook/WebHookController.java
@@ -0,0 +1,18 @@
+package com.scutech.dbackup.webhook;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class WebHookController {
+    @RequestMapping(value = "/webHooks", method = RequestMethod.POST)
+    public String webHooks(@RequestBody String data, @RequestHeader("X-DBackup2-Token") String token ) {
+        // token, verify that the request is secure
+        // 令牌，用于验证请求是否合法，排除不合法请求
+        System.out.println("Token: " + token);
+
+        // The data is JSON String
+        // data 是 JSON 字符串
+        System.out.println("Data: " + data);
+        return "";
+    }
+}

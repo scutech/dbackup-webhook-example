23a52d5650bd01b1bd36c12d682668964bc39975
zhixin <wenzhixin2010@gmail.com>
2019-05-21 18:47:37 +0800

    Updated php

--- php/webhook.php
+++ php/webhook.php
@@ -1,9 +1,10 @@
 <?php
 // token, verify that the request is secure
 // 令牌，用于验证请求是否合法，排除不合法请求
-$token = $_SERVER['X_DBACKUP2_TOKEN'];
+$token = $_SERVER['HTTP_X_DBACKUP2_TOKEN'];
+file_put_contents('token.json', $token);
 
 // The data is JSON String
 // data 是 JSON 字符串
-$data = file_get_contents("php://input");
-file_put_contents("webhooks.json", $data);
+$data = file_get_contents('php://input');
+file_put_contents('webhook.json', $data);
